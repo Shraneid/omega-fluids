c321e2fb761ae990f7e00582a75642f6539708a2
Quentin <tourette@amazon.com>
2026-03-21 12:46:11 +0100

    added prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,18 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import {defineConfig} from "eslint/config";
+import { defineConfig } from "eslint/config";
 import htmlPlugin from "@html-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { prettier },
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/ban-ts-comment": "off",
+      "prettier/prettier": "error",
     },
   },
   {
--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,85 @@
-import './style.css'
-import {SIM_SIZE, WORKGROUP_SIZE} from "./constants.ts";
+import "./style.css";
+import { SIM_SIZE, WORKGROUP_SIZE } from "./constants.ts";
 
 let current_ping_pong_buffer_index = 1;
 
 let startTime: number;
 let lastFrameTime: number;
 
+const getInitialVelocity = (x: number, y: number) => {
+    // return Math.abs(x-y) / SIM_SIZE;
+    return {
+        x: 1.0,
+        y: 0.0,
+    };
+};
+
 const startTextureData = new Float16Array(SIM_SIZE * SIM_SIZE * 4); // 4 channels (rgba)
 for (let y = 0; y < SIM_SIZE; y++) {
     for (let x = 0; x < SIM_SIZE; x++) {
         const i = (y * SIM_SIZE + x) * 4;
-        const val = y % 2 === 0 ? 1.0 : 0.0;
-        startTextureData[i + 0] = 0.0; // r
-        startTextureData[i + 1] = val; // g
-        startTextureData[i + 2] = 0.0; // b
-        startTextureData[i + 3] = 1.0; // a
+        const val = getInitialVelocity(x, y);
+        startTextureData[i + 0] = val.x; // r // right
+        startTextureData[i + 1] = val.y; // g // up
+        startTextureData[i + 2] = 0.0; // b // unused
+        startTextureData[i + 3] = 1.0; // a // unused
     }
 }
 
 // getting the HTML canvas
-const canvas: HTMLCanvasElement = document.getElementById("GLCanvas")! as HTMLCanvasElement
+const canvas: HTMLCanvasElement = document.getElementById(
+    "GLCanvas",
+)! as HTMLCanvasElement;
 
 // MAIN SETUP FOR RENDERING
 const adapter = await navigator.gpu?.requestAdapter();
 const device = await adapter?.requestDevice();
 
 if (!device) {
-    throw Error("No gpu detected")
+    throw Error("No gpu detected");
 }
 
-const context = canvas.getContext("webgpu")
-if (!context){
+const context = canvas.getContext("webgpu");
+if (!context) {
     throw Error("Error getting the context");
 }
 
-const devicePixelRatio = window.devicePixelRatio
-canvas.width = canvas.clientWidth * devicePixelRatio
-canvas.height = canvas.clientHeight * devicePixelRatio
+const devicePixelRatio = window.devicePixelRatio;
+canvas.width = canvas.clientWidth * devicePixelRatio;
+canvas.height = canvas.clientHeight * devicePixelRatio;
 
-const presentationFormat = navigator.gpu.getPreferredCanvasFormat()
+const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 context.configure({
     device: device,
     format: presentationFormat,
-})
+});
 // END MAIN SETUP FOR RENDERING
 
-
 // LOAD SHADERS
-const loadWGSL = async (path: string)=> {
-    const response = await fetch(path)
+const loadWGSL = async (path: string) => {
+    const response = await fetch(path);
     if (!response.ok) {
-        throw new Error(`Failed to load shader: ${path}`)
+        throw new Error(`Failed to load shader: ${path}`);
     }
-    return response.text()
-}
-
-const loadShaderModule = async (path: string): Promise<GPUShaderModule> => {
-    const code = await loadWGSL(path)
-    return device.createShaderModule({ code })
+    return response.text();
 };
 
-const vertexShader = await loadShaderModule('shaders/vertex.wgsl')
-const fragmentShader = await loadShaderModule('shaders/fragment.wgsl')
-const computeShader = await loadShaderModule('shaders/compute.wgsl')
-// END LOAD SHADERS
+const loadShaderModule = async (path: string): Promise<GPUShaderModule> => {
+    const code = await loadWGSL(path);
+    return device.createShaderModule({ code });
+};
 
+const vertexShader = await loadShaderModule("shaders/vertex.wgsl");
+const fragmentShader = await loadShaderModule("shaders/fragment.wgsl");
+const computeShader = await loadShaderModule("shaders/compute.wgsl");
+// END LOAD SHADERS
 
 // BUFFERS
 const uniformBuffer = device.createBuffer({
     label: "Uniform Buffer",
     size: 16,
-    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
-})
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
 
 const velocityTextureBuffers = [
     device.createTexture({
@@ -81,7 +89,7 @@ const velocityTextureBuffers = [
         usage:
             GPUTextureUsage.TEXTURE_BINDING |
             GPUTextureUsage.STORAGE_BINDING |
-            GPUTextureUsage.COPY_DST
+            GPUTextureUsage.COPY_DST,
     }),
     device.createTexture({
         label: "Velocity Texture Buffer B",
@@ -90,9 +98,9 @@ const velocityTextureBuffers = [
         usage:
             GPUTextureUsage.TEXTURE_BINDING |
             GPUTextureUsage.STORAGE_BINDING |
-            GPUTextureUsage.COPY_DST
+            GPUTextureUsage.COPY_DST,
     }),
-]
+];
 
 const pressureTextureBuffers = [
     device.createTexture({
@@ -102,7 +110,7 @@ const pressureTextureBuffers = [
         usage:
             GPUTextureUsage.TEXTURE_BINDING |
             GPUTextureUsage.STORAGE_BINDING |
-            GPUTextureUsage.COPY_DST
+            GPUTextureUsage.COPY_DST,
     }),
     device.createTexture({
         label: "Pressure Texture Buffer B",
@@ -111,70 +119,92 @@ const pressureTextureBuffers = [
         usage:
             GPUTextureUsage.TEXTURE_BINDING |
             GPUTextureUsage.STORAGE_BINDING |
-            GPUTextureUsage.COPY_DST
+            GPUTextureUsage.COPY_DST,
     }),
-]
+];
 
 const sampler = device.createSampler({
     magFilter: "linear",
     minFilter: "linear",
     addressModeU: "clamp-to-edge",
     addressModeV: "clamp-to-edge",
-})
+});
 // END BUFFERS
-
 
 // BIND GROUP LAYOUTS
 const computeBindGroupLayout = device.createBindGroupLayout({
     label: "Compute Bind Group Layout",
     entries: [
-        {binding: 0, visibility: GPUShaderStage.COMPUTE, texture: {sampleType: "float"}},
-        {binding: 1, visibility: GPUShaderStage.COMPUTE, storageTexture: {format: "rgba16float", access:"write-only"}},
-        {binding: 2, visibility: GPUShaderStage.COMPUTE, texture: {sampleType: "float"}},
-        {binding: 3, visibility: GPUShaderStage.COMPUTE, storageTexture: {format: "rgba16float", access:"write-only"}},
-        {binding: 4, visibility: GPUShaderStage.COMPUTE, sampler: {}},
-        {binding: 5, visibility: GPUShaderStage.COMPUTE, buffer: {type: "uniform"}},
-    ]
-})
+        {
+            binding: 0,
+            visibility: GPUShaderStage.COMPUTE,
+            texture: { sampleType: "float" },
+        },
+        {
+            binding: 1,
+            visibility: GPUShaderStage.COMPUTE,
+            storageTexture: { format: "rgba16float", access: "write-only" },
+        },
+        {
+            binding: 2,
+            visibility: GPUShaderStage.COMPUTE,
+            texture: { sampleType: "float" },
+        },
+        {
+            binding: 3,
+            visibility: GPUShaderStage.COMPUTE,
+            storageTexture: { format: "rgba16float", access: "write-only" },
+        },
+        { binding: 4, visibility: GPUShaderStage.COMPUTE, sampler: {} },
+        {
+            binding: 5,
+            visibility: GPUShaderStage.COMPUTE,
+            buffer: { type: "uniform" },
+        },
+    ],
+});
 
 const renderBindGroupLayout = device.createBindGroupLayout({
     label: "Render Bind Group Layout",
     entries: [
-        {binding: 0, visibility: GPUShaderStage.FRAGMENT, texture: {sampleType: "float"}},
-        {binding: 1, visibility: GPUShaderStage.FRAGMENT, sampler: {}}
-    ]
-})
+        {
+            binding: 0,
+            visibility: GPUShaderStage.FRAGMENT,
+            texture: { sampleType: "float" },
+        },
+        { binding: 1, visibility: GPUShaderStage.FRAGMENT, sampler: {} },
+    ],
+});
 // END BIND GROUP LAYOUTS
-
 
 // PIPELINES SETUP
 const computePipelineLayout = device.createPipelineLayout({
     label: "Compute Pipeline Layout",
-    bindGroupLayouts: [computeBindGroupLayout]
-})
+    bindGroupLayouts: [computeBindGroupLayout],
+});
 const renderPipelineLayout = device.createPipelineLayout({
     label: "Render Pipeline Layout",
-    bindGroupLayouts: [renderBindGroupLayout]
-})
+    bindGroupLayouts: [renderBindGroupLayout],
+});
 
 const computePipeline = device.createComputePipeline({
     label: "Compute Pipeline",
     layout: computePipelineLayout,
-    compute: {module: computeShader, entryPoint: "computeMain"},
-})
+    compute: { module: computeShader, entryPoint: "computeMain" },
+});
 
 const renderPipeline = device.createRenderPipeline({
     label: "Render Pipeline",
     layout: renderPipelineLayout,
     vertex: {
         module: vertexShader,
-        buffers: []
+        buffers: [],
     },
     fragment: {
         module: fragmentShader,
-        targets: [{format: presentationFormat}]
+        targets: [{ format: presentationFormat }],
     },
-})
+});
 
 const computeBindGroups = [
     device.createBindGroup({
@@ -205,7 +235,7 @@ const computeBindGroups = [
                 binding: 5,
                 resource: { buffer: uniformBuffer },
             },
-        ]
+        ],
     }),
     device.createBindGroup({
         label: "Compute Bind Group B",
@@ -235,9 +265,9 @@ const computeBindGroups = [
                 binding: 5,
                 resource: { buffer: uniformBuffer },
             },
-        ]
+        ],
     }),
-]
+];
 
 const renderBindGroups = [
     device.createBindGroup({
@@ -252,7 +282,7 @@ const renderBindGroups = [
                 binding: 1,
                 resource: sampler,
             },
-        ]
+        ],
     }),
     device.createBindGroup({
         label: "Render Bind Group B",
@@ -266,63 +296,77 @@ const renderBindGroups = [
                 binding: 1,
                 resource: sampler,
             },
-        ]
+        ],
     }),
-]
+];
 
 const renderPassDescriptor = {
     label: "Render Pass Description",
-    colorAttachments: [{
-        clearValue: [0.8, 0.8, 0.0, 1.0],
-        loadOp: "clear",
-        storeOp: "store",
-        view: context.getCurrentTexture().createView()
-    }]
-}
+    colorAttachments: [
+        {
+            clearValue: [0.8, 0.8, 0.0, 1.0],
+            loadOp: "clear",
+            storeOp: "store",
+            view: context.getCurrentTexture().createView(),
+        },
+    ],
+};
 // END PIPELINES SETUP
 
 // debug first state
 device.queue.writeTexture(
-    {texture: velocityTextureBuffers[0]},
+    { texture: velocityTextureBuffers[0] },
     startTextureData,
     //      rgba * float16 * ...
-    {bytesPerRow: 4 * 2 * SIM_SIZE},
-    {width: SIM_SIZE, height: SIM_SIZE}
-)
+    { bytesPerRow: 4 * 2 * SIM_SIZE },
+    { width: SIM_SIZE, height: SIM_SIZE },
+);
 
 // RENDER
-const render = (deltaTime:number, elapsedTime:number) => {
+const render = (deltaTime: number, elapsedTime: number) => {
     current_ping_pong_buffer_index = current_ping_pong_buffer_index ? 0 : 1;
 
-    renderPassDescriptor.colorAttachments[0].view = context.getCurrentTexture().createView()
+    renderPassDescriptor.colorAttachments[0].view = context
+        .getCurrentTexture()
+        .createView();
 
-    const encoder = device.createCommandEncoder({label: "command encoder"})
+    const encoder = device.createCommandEncoder({ label: "command encoder" });
 
     // UNIFORMS
-    device.queue.writeBuffer(uniformBuffer, 0, new Float32Array([deltaTime/1000, 1.0/SIM_SIZE]));
+    device.queue.writeBuffer(
+        uniformBuffer,
+        0,
+        new Float32Array([deltaTime / 1000, 1.0 / SIM_SIZE]),
+    );
 
     // COMPUTE PASS
-    const computePass = encoder.beginComputePass()
-    computePass.setPipeline(computePipeline)
-    computePass.setBindGroup(0, computeBindGroups[current_ping_pong_buffer_index])
+    const computePass = encoder.beginComputePass();
+    computePass.setPipeline(computePipeline);
+    computePass.setBindGroup(
+        0,
+        computeBindGroups[current_ping_pong_buffer_index],
+    );
 
-    const workgroupCount = Math.ceil(SIM_SIZE/WORKGROUP_SIZE)
-    computePass.dispatchWorkgroups(workgroupCount, workgroupCount)
+    const workgroupCount = Math.ceil(SIM_SIZE / WORKGROUP_SIZE);
+    computePass.dispatchWorkgroups(workgroupCount, workgroupCount);
 
-    computePass.end()
+    computePass.end();
 
     // RENDER PASS
     // @ts-ignore
     const renderPass = encoder.beginRenderPass(renderPassDescriptor);
-    renderPass.setPipeline(renderPipeline)
+    renderPass.setPipeline(renderPipeline);
 
-    renderPass.setBindGroup(0, renderBindGroups[current_ping_pong_buffer_index])
+    renderPass.setBindGroup(
+        0,
+        renderBindGroups[current_ping_pong_buffer_index],
+    );
 
-    renderPass.draw(3)
-    renderPass.end()
+    renderPass.draw(3);
+    renderPass.end();
 
-    device.queue.submit([encoder.finish()])
-}
+    device.queue.submit([encoder.finish()]);
+};
 
 const renderLoop = (timestamp: number) => {
     if (startTime === undefined) {
@@ -332,11 +376,11 @@ const renderLoop = (timestamp: number) => {
     const elapsedTime = timestamp - startTime;
     const deltaTime = timestamp - lastFrameTime;
 
-    render(deltaTime, elapsedTime)
+    render(deltaTime, elapsedTime);
 
-    lastFrameTime = timestamp
-    requestAnimationFrame(renderLoop)
-}
+    lastFrameTime = timestamp;
+    requestAnimationFrame(renderLoop);
+};
 
-requestAnimationFrame(renderLoop)
-// END RENDER 
+requestAnimationFrame(renderLoop);
+// END RENDER
